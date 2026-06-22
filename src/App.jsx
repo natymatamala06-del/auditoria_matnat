8cabc6c1bd530ccb5d8758454862e52107a62cc9
Lia Cita <natymatamala06@gmail.com>
2026-06-21 22:35:34 -0400

    feat: integrar componentes de auditoria en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
-import Resumen from "./components/Resumen";
+import InyeccionSQL from "./inyeccionSQL";
+import Xss from "./xss";
+import Comandos from "./comandos";
+import Activos from "./activos";
+import Matriz from "./matriz";
+import Controles from "./controles";
+import Recuperacion from "./recuperacion";
+import Prompts from "./prompts";
 
 function App() {
   return (
-    <>
-      <Resumen />
-    </>
+    <div>
+      <h1>Auditoría de Seguridad MatNat</h1>
+
+      <InyeccionSQL />
+      <hr />
+
+      <Xss />
+      <hr />
+
+      <Comandos />
+      <hr />
+
+      <Activos />
+      <hr />
+
+      <Matriz />
+      <hr />
+
+      <Controles />
+      <hr />
+
+      <Recuperacion />
+      <hr />
+
+      <Prompts />
+    </div>
   );
 }
 
